446cc4bbc7a58c117ff45e123a2f18f6eda2567f
Christopher Banh <chrisngobanh@gmail.com>
2015-10-23 11:36:48 -0700

    Stub Car, User, and Rating classes

--- welp/Rating.h
+++ welp/Rating.h
@@ -2,12 +2,30 @@
 #define RATING_H
 #include <string>
 
+#include "User.h";
+#include "Car.h";
+
 using namespace std;
+
+// TODO: Add a feature to allow editing Ratings
 class Rating
 {
-public:
-	Rating();
 private:
+	string description;
+	int stars;
+	User owner;
+	Car car;
+
+public:
+	// Constructor
+	Rating();
+	Rating(string _description, int _stars, User _owner, Car _car);
+
+	// Getters
+	string getDescription();
+	int getStars();
+	User getOwner();
+	Car getCar();
 };
 
 #endif
---
+++ welp/User.cpp
@@ -0,0 +1,11 @@
+#include "User.h"
+
+User::User(string _name)
+{
+	name = _name;
+}
+
+string User::getName()
+{
+	return name;
+}
